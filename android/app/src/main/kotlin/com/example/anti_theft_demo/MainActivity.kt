cf0c1f2f0d4123a2850518e7c34caafa1c3fca17
janicethomas <janice.thomas@outlook.com>
2024-03-05 00:03:05 +0530

    first commit

---
+++ android/app/src/main/kotlin/com/example/anti_theft_demo/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.anti_theft_demo
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/src/screens/home/home.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+
+import '../../settings/settings_view.dart';
+
+class HomePage extends StatefulWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  static const routeName = '/';
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Home Page"),
+        actions: [
+          IconButton(
+            icon: const Icon(Icons.settings),
+            onPressed: () {
+              // Navigate to the settings page. If the user leaves and returns
+              // to the app after it has been killed while running in the
+              // background, the navigation stack is restored.
+              Navigator.restorablePushNamed(context, SettingsView.routeName);
+            },
+          ),
+        ],
+      ),
+    );
+  }
+}
